32c72c5dcd6e40c0cfea6127606da0026d566e37
Mikey Ferguson <mikeytallyferguson@gmail.com>
2025-03-31 14:35:36 -0500

    hyperparameters

--- README.md
+++ README.md
@@ -4,3 +4,7 @@ This repository contains my implementation under the guidance of others to imple
 ## Source
 For the ```gymnasium_dql.py``` file, I followed this tutorial:<br>
 https://youtu.be/EUrWGTCGzlA?si=HBQD2hBR5k6DfYss
+
+## Additional Source
+The following repository was essential in my implementation of car racing:<br>
+https://github.com/KianAnd19/NN-car-racing/tree/main
--- car_racing.py
+++ car_racing.py
@@ -6,6 +6,8 @@ import gymnasium as gym
 import numpy as np
 import matplotlib.pyplot as plt
 
+device = "cuda" if torch.cuda.is_available() else "cpu"
+
 class CarRacingDQL(DQL):
     # Hyperparameters
     def __init__(self, learning_rate_a=0.1, learning_rate_b=0.9, replay_memory_size=100000, minibatch_size=32, network_sync_rate=50000, num_divisions=20, render=False):
@@ -24,7 +26,7 @@ class CarRacingDQL(DQL):
         if not isinstance(state, torch.Tensor):
             state = torch.FloatTensor(state)
         state = state.permute(0, 3, 1, 2) # change the order of the dimensions to (batch_size, channels, height, width)
-        return state
+        return state.squeeze()
 
     def generate_action_distribution(self):
         # Generate a random action distribution
@@ -38,6 +40,8 @@ class CarRacingDQL(DQL):
         state = obs[2]
         num_actions = env.action_space.n # 5 - none, steer left, steer right, gas, break
         self.num_actions = num_actions
+        epsilon_end = 0.01 # minimum exploration rate
+        epsilon_decay = 0.99 # decay rate for exploration
 
         epsilon = 1 # exploration rate - at first we take 100% random actions
         memory = ReplayMemory(self.replay_memory_size)
@@ -59,76 +63,80 @@ class CarRacingDQL(DQL):
         # List to keep track of epsilon decay
         epsilon_history = []
 
-        # Track number of steps taken. Used for syncing policy => target network.
-        step_count=0
-        goal_reached=False
-        best_rewards=float('-inf')
-            
-        for i in range(episodes):
-            state = env.reset()[0]  # Initialize to state 0
-            terminated = False      # True when agent falls in hole or reached goal
+        best = -np.inf
+        # Training loop
+        for epoch in range(episodes):
+            state = env.reset()[0]
+            state = self.state_to_dqn_input(state).to(device)
+            done = False
+            total_reward = 0
 
-            rewards = 0
-
-            # Agent navigates map until it falls into hole/reaches goal (terminated), or has taken 200 actions (truncated).
-            while(not terminated and rewards>-1000):
-
-                # Select action based on epsilon-greedy
-                if random.random() < epsilon:
-                    # select random action
-                    action = np.random.choice(np.arange(num_actions), p=self.action_distribution) # actions: 0=left,1=idle,2=right
+            while not done:
+                if np.random.random() < epsilon:
+                    if random.random() > 0.5:
+                        action = 3
+                    else:
+                        action = env.action_space.sample()
                 else:
-                    # select best action            
                     with torch.no_grad():
-                        action = self.policy_dqn(self.state_to_dqn_input(state)).argmax().item()
+                        q_values = self.policy_dqn(state.unsqueeze(0)).squeeze()
+                        action = q_values.cpu().numpy().argmax()
 
-                # Execute action
-                new_state,reward,terminated,_,_ = env.step(action)
+                
+                next_state, reward, done, truncated, _ = env.step(action)
+                            
+                total_reward += reward
+                memory.append((state, action, reward, next_state, done))
+                
+                if len(memory) >= self.replay_memory_size:
+                    batch = memory.sample(self.minibatch_size)
+                    states, actions, rewards, next_states, dones = zip(*batch)
 
-                # Accumulate reward
-                rewards += reward
+                    states = torch.stack(states).to(device)
+                    actions = torch.FloatTensor(np.array(actions)).to(device)
+                    rewards = torch.FloatTensor(rewards).unsqueeze(1).to(device)
+                    next_states = torch.stack(next_states).to(device)
+                    dones = torch.FloatTensor(dones).unsqueeze(1).to(device)
+                    
+                    # Predict Q-values for the current states
+                    current_q_values = self.policy_dqn(states)
+                    
+                    # Predict Q-values for the next states
+                    next_q_values = self.target_dqn(next_states)
+                    
+                    # Calculate target Q-values
+                    target_q_values = rewards + (1 - dones) * self.learning_rate_b * next_q_values
 
-                # Save experience into memory
-                memory.append((state, action, new_state, reward, terminated)) 
+                    
+                    # Compute the loss between the current Q-values and target Q-values
+                    loss = torch.nn.MSELoss()(current_q_values, target_q_values)
 
-                # Move to the next state
-                state = new_state
-
-                # Increment step counter
-                step_count+=1
-
-            # Keep track of the rewards collected per episode.
-            rewards_per_episode.append(rewards)
-            if(terminated):
-                goal_reached = True
-
-            # Graph training progress
-            if(i!=0 and i%1000==0):
-                print(f'Episode {i} Epsilon {epsilon}')
-                                        
-                self.plot_progress(rewards_per_episode, epsilon_history)
-            
-            if rewards>best_rewards:
-                best_rewards = rewards
-                print(f'Best rewards so far: {best_rewards}')
-                # Save policy
-                torch.save(self.policy_dqn.state_dict(), f"carracing_dql.pt")
-
-            # Check if enough experience has been collected
-            if len(memory)>self.minibatch_size and goal_reached:
-                self.action_distribution = self.generate_action_distribution()
-
-                mini_batch = memory.sample(self.minibatch_size)
-                self.optimize(mini_batch)        
-
-                # Decay epsilon
-                epsilon = max(epsilon - 1/episodes, 0)
-                epsilon_history.append(epsilon)
-
-                # Copy policy network to target network after a certain number of steps
-                if step_count > self.network_sync_rate:
-                    self.target_dqn.load_state_dict(self.policy_dqn.state_dict())
-                    step_count=0                    
+                    self.optimizer.zero_grad()
+                    loss.backward()
+                    # torch.nn.utils.(q_network.parameters(), max_norm=1.0)
+                    self.optimizer.step()
+                    # scheduler.step()
+                
+                state = self.state_to_dqn_input(next_state)
+                done = done or truncated
+                
+                if done:
+                    rewards_per_episode.append(total_reward)
+                    self.plot_progress(rewards_per_episode, epsilon_history)
+                    print(f"Epoch {epoch + 1}, Total Reward: {total_reward}, Epsilon: {epsilon}, Best: {best}")
+                    
+                    if best < total_reward:
+                        best = total_reward
+                        torch.save(self.policy_dqn.state_dict(), 'carracing_dql.pt')
+                    
+                    break
+                
+            if epoch % self.network_sync_rate == 0:
+                self.target_dqn.load_state_dict(self.policy_dqn.state_dict())
+                
+            epsilon = max(epsilon_end, epsilon * epsilon_decay) 
+            self.plot_progress(rewards_per_episode=rewards_per_episode, epsilon_history=epsilon_history)
+            epsilon = max(epsilon_end, epsilon * epsilon_decay)                   
 
         # Close environment
         env.close()
@@ -158,13 +166,14 @@ class CarRacingDQL(DQL):
         self.policy_dqn.load_state_dict(torch.load('carracing_dql.pt'))
         self.policy_dqn.eval() # set model to evaluation mode since not learning
         terminated = False
+        truncated = False
         for _ in range(episodes):
-            while not terminated:
+            while not terminated or truncated:
                 with torch.no_grad():
                     # Get the action from the policy
                     action = self.policy_dqn(self.state_to_dqn_input(state)).argmax().item()
                     # Take the action
-                state, _, terminated, _, _ = env.step(action)
+                state, _, terminated, truncated, _ = env.step(action)
 
         # Close environment
         env.close()
